cc6b43440a20f4cb139db2234f297c7d2a8d1a04
lzy <2397925443@qq.com>
2022-10-12 22:24:11 +0800

    feat: 根据 render 函数生成 html 并绑定到 el 指向的标签上

--- packages/core/src/lifecycle.ts
+++ packages/core/src/lifecycle.ts
@@ -1,4 +1,6 @@
 import { Component } from '#type/component'
+import { Watcher } from '@vue/observe'
+
 export function lifecycleMixin(Vue: Component) {
   Vue.prototype._update = function () {}
   Vue.prototype.$forceUpdate = function () {}
@@ -6,3 +8,19 @@ export function lifecycleMixin(Vue: Component) {
 }
 export function initLifecycle(vm: Component) {}
 
+/**
+ * 挂载组件
+ * @param vm
+ * @param el
+ */
+export function mountComponent(vm: Component, el: Element | null) {
+  const update = () => {
+    // 1. 调用 render 方法产生 虚拟DOM
+    const vnode = vm._render()
+    console.log(vnode)
+    // 2. 根据虚拟DOM 产生真实DOM
+    vm._update(vnode)
+    // 3. 插入到 el 元素中
+  }
+  new Watcher(vm, update, () => {}, {})
+}
--- packages/core/src/render.ts
+++ packages/core/src/render.ts
@@ -1,7 +1,53 @@
 import type { Component } from '#type/component'
+import { nextTick, query } from '@vue/shared'
+import { compileToFunction } from '@vue/compiler'
+import { mountComponent } from './lifecycle'
 
 export function renderMixin(Vue: Component) {
+  Vue.prototype.$mount = function (el: string | Element) {
+    el = query(el)
+    const opts = this.$options
+    // 当 render 函数不存在时，render > template > el
+    // 当存在时，
+    if (!opts.render) {
+      let template = opts.template
+
+      if (template) {
+        // 以 # 开始，会被作为选择符，并使用匹配元素的 innerHTML 作为模版
+        if (typeof template === 'string' && template.charAt(0) === '#') {
+          template = query(template).innerHTML
+        }
+        //  else {
+
+        // }
+      } else if (el) {
+        template = el.outerHTML
+      }
+
+      if (template) {
+        const render = compileToFunction(template)
+        opts.render = render
+      }
+    }
+    return mountComponent(this, el)
+  }
+
   Vue.prototype._render = function () {
+    const vm: Component = this
+
+    const { render } = vm.$options
+    let vnode
+    try {
+      vnode = render.call(vm, vm.$createElement)
+    } catch (error) {
+      console.error(error)
+    }
+    return vnode
+  }
+
+  Vue.prototype.$nextTick = function (fn: Function) {
+    return nextTick(fn, this)
+  }
   }
 
 export function initRender(vm: Component) {
