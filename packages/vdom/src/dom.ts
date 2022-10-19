e5c8ea1f863e94ae9037f31f025b28e10081c4c0
lzy <2397925443@qq.com>
2022-10-20 07:35:07 +0800

    feat: 完成初始由 vnode 到真实 DOM 的转变，但是原始 DOM 未移除

--- packages/vdom/src/dom.ts
+++ packages/vdom/src/dom.ts
@@ -1,6 +1,61 @@
-export function createElement(
-  vnode,
-  insertVnodeQueue,
-  parentElm?: any,
-  refELm?: any
-) {}
+import { isDef } from '@vue/shared'
+import { VNode } from './vnode'
+
+/**
+ * 创建元素
+ * @param tag 标签名
+ * @param vnode VNode
+ * @returns ELement
+ */
+export function createElement(tag: string, vnode: VNode): Element {
+  const el = document.createElement(tag)
+
+  return el
+}
+
+/**
+ * 创建文本节点
+ * @param text
+ * @returns Text
+ */
+export function createText(text: string): Text {
+  return document.createTextNode(text)
+}
+/**
+ * 添加子节点
+ * @param el 父节点
+ * @param child 子节点
+ */
+export function appendChild(el: Node, child: Node) {
+  el.appendChild(child)
+}
+/**
+ * 插入到参考节点之前
+ * @param parentNode 父节点
+ * @param newNode 新元素节点
+ * @param refNode 参考节点
+ */
+export function insertBefore(parentNode: Node, newNode: Node, refNode: Node) {
+  parentNode.insertBefore(newNode, refNode)
+}
+
+/**
+ * 移除子节点
+ * @param node 父节点
+ * @param child 子节点
+ */
+export function removeChild(node: Node, child: Node) {
+  node.removeChild(child)
+}
+
+export function nextSibling(node: Node) {
+  return node.nextSibling
+}
+
+export function parentNode(node: Node) {
+  return node.parentNode
+}
+
+export function tagName(node: Element): string {
+  return node.tagName
+}
--- packages/vdom/src/patch.ts
+++ packages/vdom/src/patch.ts
@@ -1,14 +1,67 @@
 import { isDef } from '@vue/shared'
-import { createElement } from './dom'
+import {
+  appendChild,
+  createElement,
+  createText,
+  insertBefore,
+  nextSibling,
+  parentNode,
+  tagName,
+} from './dom'
+import { VNode } from './vnode'
+
+function createElm(vnode, insertVnodeQueue, parentElm?: any, refELm?: any) {
+  const tag = vnode.tag
+  // 标签存在
+  if (isDef(tag)) {
+    vnode.elm = createElement(tag, vnode)
+    // 创建子元素
+    createChildren(vnode, vnode.children, insertVnodeQueue)
+    // 插入元素
+    insert(parentElm, vnode.elm, refELm)
+  } else {
+    // 文本
+    vnode.elm = createText(vnode.text!)
+    // 插入元素
+    insert(parentElm, vnode.elm, refELm)
+  }
+}
+
+function createChildren(vnode, children, insertVnodeQueue) {
+  if (Array.isArray(children)) {
+    for (let i = 0; i < children.length; i++) {
+      createElm(children[i], insertVnodeQueue, vnode.elm)
+    }
+  } else {
+    const text = createText(vnode.text)
+    appendChild(vnode.elm, text)
+  }
+}
+
+function insert(parent, elm, ref) {
+  if (isDef(parent)) {
+    if (isDef(ref) && parentNode(ref) === parent) {
+      insertBefore(parent, elm, ref)
+    } else {
+      appendChild(parent, elm)
+    }
+  }
+}
 
 export function patch(preVnode, vnode) {
   const isRealEl = isDef(preVnode.nodeType)
   const insertVnodeQueue: any[] = []
   if (isRealEl) {
+    preVnode = new VNode(
+      tagName(preVnode).toLowerCase(),
+      {},
+      [],
+      undefined,
+      preVnode
+    )
     // 是真实元素，则根据 vnode 创建元素
-    const parentElm = preVnode.parentNode
-
-    createElement(vnode, insertVnodeQueue, parentElm)
+    const parentElm = parentNode(preVnode.elm)
+    createElm(vnode, insertVnodeQueue, parentElm, nextSibling(parentElm!))
   } else {
     // 是虚拟节点，则 diff
   }
