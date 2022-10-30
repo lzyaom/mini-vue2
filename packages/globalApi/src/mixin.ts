b771225f42aff5c7b2da2dbb96fc560aab9ec20d
lzy <2397925443@qq.com>
2022-10-30 23:04:40 +0800

    feat: 添加全局 mixin 函数

--- packages/globalApi/src/mixin.ts
+++ packages/globalApi/src/mixin.ts
@@ -1,4 +1,13 @@
+import { mergeOptions } from '@vue/shared'
+
 export function initMixin(Vue) {
+  /**
+   * 将传入的对象 混入 到全局 options
+   * @param mixin
+   * @returns
+   */
   Vue.mixin = function (mixin: Object) {
+    this.options = mergeOptions(this.options, mixin)
+    return this
   }
 }
